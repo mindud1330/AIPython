9324188063f18fdce5981f9bab5132347b317cb9
mindud1330 <mindud1330@naver.com>
2024-05-17 12:31:32 +0900

    Update 20240517-AIPython-m3-3.py

--- 20240517-AIPython-m3-3.py
+++ 20240517-AIPython-m3-3.py
@@ -5,10 +5,3 @@ penguins_data = sns.load_dataset("penguins")
 
 sns.regplot(penguins_data, x='flipper_length_mm', y='body_mass_g')
 plt.show()
-
-numeric_penguins_data = penguins_data.select_dtypes(include=['float64', 'int64'])
-
-correlation_matrix = numeric_penguins_data.corr()
-
-sns.heatmap(data=correlation_matrix, annot=True, fmt='.2f', cbar=False)
-plt.show()
